e4b08b12ec3dac6fd4b70b7a2e3ddb64cb74b708
naoki1117 <jngmtpmdgaugad@gmail.com>
2024-05-12 18:09:03 +0900

    wrgrqr

--- app/components/WelcomeComponent.tsx
+++ app/components/WelcomeComponent.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+"use client";
 
-const WelcomeComponent = () => {
+import React, { useState, useEffect } from "react";
+import { Transition } from "@headlessui/react";
+
+const AnimatedText = () => {
+  const [showText, setShowText] = useState(false);
+  const text = "Welcome To fast-vote!";
+
+  useEffect(() => {
+    setShowText(true);
+  }, []);
+
   return (
-    <div className="flex justify-center items-center h-[500px] text-3xl">
-      <h1 className="text-center">Welcome To fast-vote!</h1>
+    <div className="text-center">
+      <Transition
+        show={showText}
+        as="h1"
+        enter="transition-opacity duration-1000"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+      >
+        {text.split("").map((char, index) => (
+          <Transition.Child
+            key={index}
+            as="span"
+            enter="transition-opacity duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+            {char}
+          </Transition.Child>
+        ))}
+      </Transition>
     </div>
   );
 };
 
-export default WelcomeComponent;
+export default AnimatedText;
